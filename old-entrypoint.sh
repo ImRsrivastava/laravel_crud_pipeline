d8d6f3c1ed3e74962aa25553cd6188d4c2e9eaef
Rishabh Srivastava <rishabh.sr@cisinlabs.com>
2025-02-17 13:25:57 +0530

    modified files

---
+++ old-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Waiting for MySQL to be ready..."
+sleep 10
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Clear Laravel caches and optimize..."
+php artisan config:cache
+php artisan cache:clear
+php artisan route:cache
+
+# Capture the port from the .env file or use the default 8081 from docker-compose.yml
+PORT=$(grep -oP '(?<=^CONTAINER_PORT=).*' .env)
+PORT=${PORT:-8000}  # Default to 8000 if not set in .env
+
+echo "Starting Laravel project on port $PORT..."
+exec php artisan serve --host 0.0.0.0 --port $PORT
+
+echo "Starting Apache..."
+exec apache2-foreground
+
